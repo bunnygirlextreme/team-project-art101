fc4476fc8ec23d7e7899ba0ab79196d3a9d94c3a
odette <odegarci@ucsc.edu>
2025-05-19 22:47:18 -0700

    Squirrely added. Changed some code to fit in with the squirrely pop-up i made
    Some of the code might be janky and the WesBot offered only limited help. I dont do too good with jQuery but if there is a way to simplify the Squirrely , then please tell me! feel free to fix anything

--- demos/js/lab.js
+++ demos/js/lab.js
@@ -12,37 +12,32 @@ function openPopup2() {
 }
 
 // Closes all popups
+
 function closeAll() {
+  const overlay = document.getElementById("overlay");
   const popup = document.getElementById("popup");
   const popup2 = document.getElementById("popup2");
-  const overlay = document.getElementById("overlay");
+  const popup3 = document.getElementById("popup3");
 
-  if (popup) {
-    popup.style.display = "none";
-  }
+  if (popup) popup.style.display = "none";
+  if (popup3) popup3.style.display = "none";
 
-  if (popup2) {
-   
-    $(popup2).fadeOut(2300);
-
-    setTimeout(() => {
-      overlay.style.display = "none";
-    }, 2300);
-    
-    const sound = new Audio('sounds/smited.mov'); 
+  if (popup2 && popup2.style.display !== "none") {
+    const sound = new Audio('sounds/smited.mov');
     sound.play();
 
-    //popup2.style.display = "none";
-    return;
-  }
-
-  
-  if (overlay) {
-    overlay.style.display = "none";
+    $(popup2).fadeOut(2300, function () {
+      
+      if (overlay) overlay.style.display = "none";
+    });
+  } else {
+    
+    if (overlay) overlay.style.display = "none";
   }
 }
 
-// Main popup maker
+
+  // Main popup maker
 
 function openRandomPopup() {
   const overlay = document.getElementById("overlay");
@@ -63,6 +58,7 @@ function openRandomPopup() {
 
 
 // Captcha func
+
 // click listener for button
 $("#captcha").click(function(){
 
@@ -105,6 +101,60 @@ document.addEventListener("mousemove", function(e) {
 
     btn.style.transform = `scale(${scale})`;
 });
+
+// Squirrely Pop Up Functions/Handles (this could be simplified with jQuery but for now this is what I have done with vanilla JS)
+
+function openPopup3() { // Shows popup at a random position
+  const popup3 = document.getElementById("popup3");
+  const closeBtn = document.querySelector(".close-btn");
+  const photo = document.querySelector(".photo");
+
+  const maxX = window.innerWidth - 320; // leaves some margin
+  const maxY = window.innerHeight - 320;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  popup3.style.left = `${randomX}px`;
+  popup3.style.top = `${randomY}px`;
+  popup3.style.display = "block";
+
+  photo.style.width = popup3.style.width;  // Match width of the popup(not needed unless you want to tbh)
+  photo.style.height = popup3.style.height; // Match height of the popup (not needed unless you want the image to be the same as the pop up made)
+
+
+  closeBtn.style.position = "absolute";
+  closeBtn.style.top = "10px";
+  closeBtn.style.right = "10px";
+}
+
+document.addEventListener("DOMContentLoaded", function () { // Move popup randomly when hovering over the image
+  const photo = document.querySelector(".photo");
+  const popup3 = document.getElementById("popup3");
+
+  popup3.style.position = "absolute"; //size and position of pop-up (changing it within bounds of javascript in order to not change the css)
+  popup3.style.width = "300px";
+  popup3.style.height = "300px";
+
+  $('.hover-trigger').on('mouseenter', function () { // Hover trigger (important for many of the pop-ups we are going to make)
+    openPopup3();
+  });
+
+  document.querySelector(".close-btn").addEventListener("click", closeAll);
+
+  
+  photo.addEventListener("mouseover", function () { // Makes it so that when the mouse is hovered over, the pop-up appears randomly in page
+    const maxX = window.innerWidth - popup3.offsetWidth;
+    const maxY = window.innerHeight - popup3.offsetHeight;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    popup3.style.left = `${randomX}px`;
+    popup3.style.top = `${randomY}px`;
+  });
+});
+
 
 
 // ------------------------------------------------------
